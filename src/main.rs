7027503e6ab98e6e0b2864a3e19190c5bf6fb28d
jmeaster30 <jhneasterday09@gmail.com>
2022-04-02 16:13:44 -0400

    Finished decoder and started on the encoder.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,126 @@
 mod qoi;
 
-fn main() {
-    println!("Hello, world!");
-    qoi::qoi_decode(&[66, 79, 79, 66, 0, 0, 0, 60, 0, 0, 0, 60, 1, 1]);
+use pixel_canvas::{Canvas, Color, input::MouseState};
+use glob::glob;
+use std::cmp;
+use std::io;
+use std::time::Instant;
+
+fn first_mismatch(a: Vec<u8>, b: Vec<u8>) -> usize
+{
+    let longest = if a.len() > b.len() { a.len() } else { b.len() };
+    for i in 0..longest {
+        if i >= a.len() || i >= b.len() {
+            return i;
+        }
+        let av = a[i];
+        let bv = b[i];
+        if av != bv {
+            return i;
+        }
+    }
+    return longest;
+}
+
+fn main() -> io::Result<()>{
+
+    let command = std::env::args().nth(1).expect("Missing command. Expected 'test', 'encode', or 'decode'");
+
+    match command.as_str() {
+        "test" => {
+            let mut pass = 0; 
+            let mut total = 0;
+
+            for glob_entry in glob("qoi_test_images/testcard.qoi").unwrap() {
+                match glob_entry {
+                    Ok(file) => {
+                        let filename = &file.into_os_string().into_string().unwrap();
+
+                        println!("\nLoading file {}", filename);
+
+                        let qfile = qoi::QoiFile::load_from_file(filename)?;
+                        let initial_encoded = qfile.encoded.clone();
+
+                        let now = Instant::now();
+                        let decoded = qfile.decode();
+                        let elapsed = now.elapsed();
+
+                        //let w = decoded.width as usize;
+                        //let h = decoded.height as usize;
+
+                        println!("Finished decoding image '{}'\tElapsed: {:.2?}", filename, elapsed);
+                        //println!("Decoded Length {} - W {} x H {}", decoded.decoded.len(), w, h);
+
+                        let now = Instant::now();
+                        let final_encoded = decoded.encode();
+                        let elapsed = now.elapsed();
+
+                        println!("Finished encoding image '{}'\tElapsed: {:.2?}", filename, elapsed);
+
+                        if initial_encoded == final_encoded.encoded {
+                            println!("{} - PASS", filename);
+                            pass += 1;
+                        } else {
+                            println!("{} - FAILED", filename);
+                        }
+
+                        let distance = 10;
+                        let first_fail_idx = first_mismatch(initial_encoded.clone(), final_encoded.encoded.clone());
+                        let i_min = 0;//first_fail_idx - distance;
+                        let i_max = cmp::min(first_fail_idx + distance, initial_encoded.len());
+                        let f_min = 0;//first_fail_idx - distance;
+                        let f_max = cmp::min(first_fail_idx + distance, final_encoded.encoded.len());
+                        println!("{}", first_fail_idx);                        
+                        println!("{} {}", i_min, i_max);
+                        println!("{} {}", f_min, f_max);
+
+                        println!("{:?}", &(initial_encoded[i_min..i_max]));
+                        println!("{:?}", &(final_encoded.encoded[f_min..f_max]));
+
+                        total += 1;
+                    },
+                    Err(e) => println!("{:?}", e)
+                }
+                
+            }
+
+            println!("{} out of {} pics passed.", pass, total);
+        },
+        "decode" => {
+            let filename = std::env::args().nth(2).expect("Missing filename to decode");
+            let qoifile = qoi::QoiFile::load_from_file(&filename)?;
+            
+            let now = Instant::now();
+            let decoded = qoifile.decode();
+            let elapsed = now.elapsed();
+
+            let w = decoded.width as usize;
+            let h = decoded.height as usize;
+
+            println!("Finished decoding image '{}'\tElapsed: {:.2?}", filename, elapsed);
+
+            let canvas = Canvas::new(w, h)
+                .title(filename)
+                .state(MouseState::new())
+                .input(MouseState::handle_input);
+
+            canvas.render(move |_mouse, image| {
+                for (y, row) in image.chunks_mut(w).enumerate() {
+                    for (x, pixel) in row.iter_mut().enumerate() {
+                        *pixel = Color {
+                            r: decoded.get(x, h - y).red,
+                            g: decoded.get(x, h - y).green,
+                            b: decoded.get(x, h - y).blue,
+                        }
+                    }
+                }
+            });
+        }
+        "encode" => {
+            println!("encode");
+        },
+        _ => println!("Unknown command {}. Expected 'test', 'encode', or 'decode'", command)
+    }
+
+    Ok(())
 }
--- src/qoi.rs
+++ src/qoi.rs
@@ -1,49 +1,349 @@
 use std::convert::TryInto;
+use std::vec;
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
 
-/*
-QOI Header
+const QOI_OP_INDEX: u8 = 0x00; /* 00xxxxxx */
+const QOI_OP_DIFF: u8  = 0x40; /* 01xxxxxx */
+const QOI_OP_LUMA: u8  = 0x80; /* 10xxxxxx */
+const QOI_OP_RUN: u8   = 0xc0; /* 11xxxxxx */
+const QOI_OP_RGB: u8   = 0xfe; /* 11111110 */
+const QOI_OP_RGBA: u8  = 0xff; /* 11111111 */
 
-char[4] magic     <- 4 bytes
-uint32 width      <- 4 bytes
-uint32 height     <- 4 bytes
-uint8 channels    <- 1 byte
-uint8 colorspace  <- 1 byte
-
-*/
-
-#[derive(Debug, Clone)]
-struct QoiEncodeError;
-
-#[derive(Debug, Clone)]
-struct QoiDecodeError;
-
-
-pub fn qoi_encode(bytes: &[u8], width: u32, height: u32) -> &[u8]
-{
-  &[10]
+#[derive(Debug, Clone, PartialEq)]
+pub enum QoiChannels {
+  UNK, RGB, RGBA
 }
 
-pub fn qoi_decode(bytes: &[u8]) -> (u32, u32, &[u8])
-{
-  // check that we have enough bytes for the header at least
-  if bytes.len() < 14 {
-    panic!("Not enough bytes for a valid qoi file");
+#[derive(Debug, Clone, PartialEq)]
+pub enum QoiColorspace {
+  UNK, SRGB, LINEAR
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct QoiPixel {
+  pub red: u8,
+  pub green: u8,
+  pub blue: u8,
+  pub alpha: u8,
+}
+
+impl QoiPixel {
+
+  pub fn empty() -> QoiPixel
+  {
+    QoiPixel { red: 0, green: 0, blue: 0, alpha: 255 }
   }
 
-  let magic = match std::str::from_utf8(&bytes[0..4]) {
-    Ok(s) => s,
-    Err(e) => panic!("Invalid UTF-8 sequence: {}", e)
-  };
+  pub fn clear() -> QoiPixel
+  {
+    QoiPixel { red: 0, green: 0, blue: 0, alpha: 0 }
+  }
 
-  let width = u32::from_be_bytes((&bytes[4..8]).try_into().expect("slice with incorrect length"));
-  let height = u32::from_be_bytes((&bytes[8..12]).try_into().expect("slice with incorrect length"));
-  let channels = &bytes[12];
-  let colorspace = &bytes[13];
+  pub fn new(red: u8, green: u8, blue: u8, alpha: u8) -> QoiPixel
+  {
+    QoiPixel { red: red, green: green, blue: blue, alpha: alpha }
+  }
 
-  println!("{}", magic);
-  println!("{}", width);
-  println!("{}", height);
-  println!("{} - {}", channels, colorspace);
+  pub fn hash(&self) -> usize
+  {
+    (self.red as usize * 3 + self.green as usize * 5 + self.blue as usize * 7 + self.alpha as usize * 11) % 64
+  }
+}
 
-  (width, heightgit co, &[10])
+#[derive(Debug, Clone)]
+pub struct QoiFile {
+  pub encoded: Vec<u8>,
+  pub decoded: Vec<QoiPixel>,
+  pub width: u32,
+  pub height: u32,
+  pub channels: QoiChannels,
+  pub colorspace: QoiColorspace,
+}
+
+impl QoiFile
+{
+  pub fn load_from_file(filename: &str) -> io::Result<QoiFile>
+  {
+    let mut bytes = Vec::new();
+    let mut f = File::open(filename)?;
+
+    f.read_to_end(&mut bytes)?;
+
+    Ok(QoiFile { encoded: bytes, decoded: Vec::new(), width: 0, height: 0, channels: QoiChannels::RGB, colorspace: QoiColorspace::LINEAR })
+  }
+
+  pub fn get(&self, x: usize, y: usize) -> QoiPixel
+  {
+    if self.decoded.len() <= y * (self.width as usize) + x {
+      return QoiPixel::empty();
+    }
+    self.decoded[y * (self.width as usize) + x].clone()
+  }
+
+  pub fn encode(mut self) -> QoiFile
+  {
+    self.encoded = Vec::new();
+
+    self.encoded.push('q' as u8);
+    self.encoded.push('o' as u8);
+    self.encoded.push('i' as u8);
+    self.encoded.push('f' as u8);
+
+    self.encoded.append(&mut write_32(self.width));
+    self.encoded.append(&mut write_32(self.height));
+    self.encoded.push(match self.channels {
+      QoiChannels::RGB => 3,
+      QoiChannels::RGBA => 4,
+      QoiChannels::UNK => 1,
+    });
+
+    self.encoded.push(match self.colorspace {
+      QoiColorspace::SRGB => 0,
+      QoiColorspace::LINEAR => 1,
+      QoiColorspace::UNK => 2,
+    });
+
+    let mut running_array = vec![QoiPixel::clear(); 64];
+    let mut previous_pixel = QoiPixel::empty();
+    let mut run_length: u8 = 0;
+    for pixel in &self.decoded
+    {
+      if pixel.clone() == previous_pixel {
+        run_length += 1;
+        if run_length == 62 {
+          self.encoded.push(QOI_OP_RUN | (run_length - 1));
+          run_length = 0;
+        }
+      }
+      else
+      {
+        if run_length > 0
+        {
+          self.encoded.push(QOI_OP_RUN | (run_length - 1));
+          run_length = 0;
+        }
+
+        if running_array[pixel.hash()] == pixel.clone()
+        {
+          self.encoded.push(QOI_OP_INDEX | pixel.hash() as u8);
+        }
+        else
+        {
+          running_array[pixel.hash()] = pixel.clone();
+
+          if pixel.alpha == previous_pixel.alpha
+          {
+            let vr: i32 = (pixel.red as i32) - (previous_pixel.red as i32);
+            let vg: i32 = (pixel.green as i32) - (previous_pixel.green as i32);
+            let vb: i32 = (pixel.blue as i32) - (previous_pixel.blue as i32);
+  
+            let vg_r: i32 = vr - vg;
+            let vg_b: i32 = vb - vg;
+
+            println!("[{}] {}, {}, {}, {} -- {}, {}, {}, {}", self.encoded.len(), pixel.red, pixel.green, pixel.blue, pixel.alpha, previous_pixel.red, previous_pixel.green, previous_pixel.blue, pixel.alpha);
+            println!("[{}] {}, {}, {} -- {}, {}", self.encoded.len(), vr, vg, vb, vg_r, vg_b);
+
+            if vr > -3 && vr < 2 && vg > -3 && vg < 2 && vb > -3 && vb < 2
+            {
+              println!("diff");
+              self.encoded.push(QOI_OP_DIFF | (((vr + 2) as u8) << 4) | (((vg + 2) as u8) << 2) | (vb + 2) as u8);
+            }
+            else if vg_r > -9 && vg_r < 8 && vg > -33 && vg < 32 && vg_b > -9 && vg_b < 8
+            {
+              println!("luma");
+              self.encoded.push(QOI_OP_LUMA | (vg + 32) as u8);
+              self.encoded.push((((vg_r + 8) as u8) << 4) | (vg_b + 8) as u8);
+            }
+            else
+            {
+              println!("rgb");
+              self.encoded.push(QOI_OP_RGB);
+              self.encoded.push(pixel.red);
+              self.encoded.push(pixel.green);
+              self.encoded.push(pixel.blue);
+            }
+          }
+          else
+          {
+            self.encoded.push(QOI_OP_RGBA);
+            self.encoded.push(pixel.red);
+            self.encoded.push(pixel.green);
+            self.encoded.push(pixel.blue);
+            self.encoded.push(pixel.alpha);
+          }
+        }
+        
+      }
+
+      previous_pixel = pixel.clone();
+    }
+
+    if run_length > 0 {
+      self.encoded.push(QOI_OP_RUN | (run_length - 1));
+    }
+
+    self.encoded.push(0);
+    self.encoded.push(0);
+    self.encoded.push(0);
+    self.encoded.push(0);
+    self.encoded.push(0);
+    self.encoded.push(0);
+    self.encoded.push(0);
+    self.encoded.push(1);
+
+    self
+  }
+
+  pub fn decode(mut self) -> QoiFile
+  {
+    // check that we have enough bytes for the header at least
+    if self.encoded.len() < 14 {
+      panic!("Not enough bytes for a valid qoi file :(");
+    }
+
+    let magic = match std::str::from_utf8(&self.encoded[0..4]) {
+      Ok(s) => s,
+      Err(e) => panic!("Invalid UTF-8 sequence '{:?}': {}", &self.encoded[0..4], e)
+    };
+
+    if magic != "qoif" {
+      panic!("Expected magic number 'qoif' but got '{}' :(", magic)
+    }
+
+    self.width = u32::from_be_bytes((&self.encoded[4..8]).try_into().expect("slice with incorrect length"));
+    self.height = u32::from_be_bytes((&self.encoded[8..12]).try_into().expect("slice with incorrect length"));
+    self.channels = match &self.encoded[12] {
+      3 => QoiChannels::RGB,
+      4 => QoiChannels::RGBA,
+      _ => QoiChannels::UNK
+    };
+    self.colorspace = match &self.encoded[13] {
+      0 => QoiColorspace::SRGB,
+      1 => QoiColorspace::LINEAR,
+      _ => QoiColorspace::UNK
+    };
+
+    self.decoded = Vec::with_capacity(self.width as usize * self.height as usize);
+  
+    let mut running_array = vec![QoiPixel::clear(); 64];
+    let mut previous_pixel = QoiPixel::empty();
+
+    let mut i = 14;
+    while i < self.encoded.len() - 8 {
+      let current_byte = self.encoded[i];
+      let current_pixel: QoiPixel;
+      let mut run_length: u8 = 1;
+
+      if is_op_rgb(current_byte) {
+        let next_bytes = &self.encoded[(i + 1)..(i + 4)];
+        i += 3;
+        current_pixel = QoiPixel::new(next_bytes[0], next_bytes[1], next_bytes[2], previous_pixel.alpha);
+      } else if is_op_rgba(current_byte) {
+        let next_bytes = &self.encoded[(i + 1)..(i + 5)];
+        i += 4;
+        current_pixel = QoiPixel::new(next_bytes[0], next_bytes[1], next_bytes[2], next_bytes[3]);
+      } else if is_op_index(current_byte) {
+        current_pixel = running_array[current_byte as usize].clone();
+      } else if is_op_diff(current_byte) {
+        let dr = sub((current_byte & 48) >> 4, 2);
+        let dg = sub((current_byte & 12) >> 2, 2);
+        let db = sub(current_byte & 3, 2);
+        current_pixel = QoiPixel::new(add(previous_pixel.red, dr), add(previous_pixel.green, dg), add(previous_pixel.blue, db), previous_pixel.alpha);
+      } else if is_op_luma(current_byte) {
+        let diff_green = sub(current_byte & 63, 32);
+        let next_byte = &self.encoded[i + 1];
+        i += 1;
+        let drdg = (next_byte >> 4) & 15;
+        let dbdg = next_byte & 15;
+        current_pixel = QoiPixel::new(add3(previous_pixel.red, sub(drdg, 8), diff_green), add(previous_pixel.green, diff_green), add3(previous_pixel.blue, sub(dbdg, 8), diff_green), previous_pixel.alpha);
+      } else if is_op_run(current_byte) {
+        current_pixel = previous_pixel.clone();
+        run_length = (current_byte & 63) + 1;
+      } else {
+        panic!("Unexpected byte {:?}", current_byte);
+      }
+
+      running_array[current_pixel.hash()] = current_pixel.clone();
+
+      while run_length > 0 {
+        self.decoded.push(current_pixel.clone());
+        run_length -= 1;
+      }
+
+      previous_pixel = current_pixel.clone();
+      i += 1;
+    }
+
+    let padding = [0, 0, 0, 0, 0, 0, 0, 1];
+    while i < self.encoded.len() {
+      if self.encoded[i] != padding[i + 8 - self.encoded.len()] 
+      {
+        panic!("There is an error in the padding :(");
+      };
+      i += 1;
+    }
+
+    self
+  }
+}
+
+// helper functions 
+fn add(a: u8, b: u8) -> u8
+{
+  (((a as u32) + (b as u32)) % 256) as u8
+}
+
+fn add3(a: u8, b: u8, c: u8) -> u8
+{
+  (((a as u32) + (b as u32) + (c as u32)) % 256) as u8
+}
+
+fn sub(a: u8, b: u8) -> u8
+{
+  (((a as u32) + 256 - (b as u32)) % 256) as u8
+}
+
+fn is_op_rgb(byte: u8) -> bool
+{
+  byte == 254
+}
+
+fn is_op_rgba(byte: u8) -> bool
+{
+  byte == 255
+}
+
+fn is_op_index(byte: u8) -> bool
+{
+  (byte & 192) == 0
+}
+
+fn is_op_diff(byte: u8) -> bool
+{
+  (byte & 192) == 64
+}
+
+fn is_op_luma(byte: u8) -> bool
+{
+  (byte & 192) == 128
+}
+
+fn is_op_run(byte: u8) -> bool
+{
+  (byte & 192) == 192
+}
+
+
+fn write_32(v: u32) -> Vec<u8>
+{
+  let mut split = Vec::new();
+
+  split.push(((0xff000000 & v) >> 24) as u8);
+  split.push(((0x00ff0000 & v) >> 16) as u8);
+  split.push(((0x0000ff00 & v) >> 8) as u8);
+  split.push((0x000000ff & v) as u8);
+
+  split
 }
